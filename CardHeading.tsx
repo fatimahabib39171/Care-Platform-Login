a9f54d009b2dd649411436aa9279ba4a38eea5e0
Fatima Habib <151499011+fatimahabib39171@users.noreply.github.com>
2026-08-17 11:55:53 -0700

    Add files via upload

---
+++ CardHeading.tsx
@@ -0,0 +1,44 @@
+import { colorPlater, font } from "@/theme/theme";
+import { StyleSheet, Text, TextInputProps, View } from "react-native";
+
+type HeadingProps = TextInputProps & {
+  heading: string;
+  subheading: string;
+};
+
+export default function CardHeading({
+  heading,
+  subheading,
+  ...inputProps
+}: HeadingProps) {
+  return (
+    <View>
+      <Text style={styles.cardHeading}>{heading}</Text>
+      <Text style={styles.cardSubheading}>{subheading}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  cardHeading: {
+    color: colorPlater.color.Primary,
+    fontFamily: font.family,
+    fontSize: 17,
+    fontStyle: "normal",
+    fontWeight: 700,
+    lineHeight: 20.4,
+
+    marginTop: 20,
+    marginBottom: 3,
+  },
+  cardSubheading: {
+    color: colorPlater.color.footer,
+    fontFamily: font.family,
+    fontSize: 13,
+    fontStyle: "normal",
+    fontWeight: 400,
+    lineHeight: 15.6,
+
+    marginBottom: 17,
+  },
+});
